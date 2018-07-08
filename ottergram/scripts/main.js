c6a5e75d7326d50757cdd3f5c1d4eddbfb3cd053
ecshenryho <ecshenryho@gmail.com>
2018-07-08 10:49:39 -0700

    show image by clicking

--- ottergram/scripts/main.js
+++ ottergram/scripts/main.js
@@ -1,7 +1,7 @@
 var DETAIL_IMAGE_SELECTOR='[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR='[data-image-role="title"]';
 var THUMBNAIL_LINK_SELECTOR='[data-image-role="trigger"]';
-
+var HIDDEN_DETAIL_CLASS='hidden-detail';
 function setDetails(imageUrl,titleText){
   'use strict';
   var detailImage=document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -23,3 +23,22 @@ function titleFromThumb(thumbnail){
 function setDetailsFromThumb(thumbnail) {
   setDetails(imageFromThumb(thumbnail),titleFromThumb(thumbnail));
 }
+function addThumbClickHandler(thumb) {
+  'use strict';
+  thumb.addEventListener('click',function(event){
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+  });
+}
+function getThumbnailsArray() {
+  'use strict';
+  var thumbnails=document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailArray=[].slice.call(thumbnails);
+  return thumbnailArray;
+}
+function initializeEvents() {
+  'use strict';
+  var thumbnails=getThumbnailsArray();
+  thumbnails.forEach(addThumbClickHandler);
+}
+initializeEvents();
